6cb5c1113eef41331c261660e1b6e12486a4a5a8
venes <victor.neves4@fatec.sp.gov.br>
2024-06-27 17:46:44 -0300

    feat: configure lsp and linter to js

--- packages/nvim/.config/nvim/lua/plugins/none-ls.lua
+++ packages/nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,9 +3,11 @@ return {
   config = function()
     local null_ls = require("null-ls")
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
       },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
